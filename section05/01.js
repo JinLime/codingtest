8a1db56332cd5f3472357b5acad23e3787c8ff08
sj <bsj6551@naver.com>
2022-02-14 18:11:34 +0900

    01~02

--- section05/01.js
+++ section05/01.js
@@ -1,9 +1,23 @@
-const  = () => {
-    let answer;
-    
+const arrSum = (arr1, arr2) => {
+    let answer = [];
+    let n = arr1.length,
+        m = arr2.length;
+    let p1 = 0,
+        p2 = 0;
+    while (p1 < n && p2 < m) {
+        if (arr1[p1] <= arr2[p2]) {
+            answer.push(arr1[p1++]);
+        } else {
+            answer.push(arr2[p2++]);
+        }
+    }
+    while (p1 < n) answer.push(arr1[p1++]);
+    while (p2 < m) answer.push(arr2[p2++]);
+
     return answer;
 };
 
-arr = [];
+let arr1 = [1, 3, 5];
+let arr2 = [2, 3, 6, 7, 9];
 
-console.log();
+console.log(arrSum(arr1, arr2));
--- section05/02.js
+++ section05/02.js
@@ -1,9 +1,14 @@
-const  = () => {
-    let answer;
-    
-    return answer;
+const getCommonElement = (arr1, arr2) => {
+    let answer = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr2.indexof(arr1[i])) answer.push(arr1[i]);
+    }
+
+    return answer.sort();
 };
 
-arr = [];
+let arr1 = [1, 3, 9, 5, 2];
+let arr2 = [3, 2, 5, 7, 8];
 
-console.log();
+console.log(getCommonElement(arr1, arr2));
