286df0b42883522f2b7df699e73601187985989d
DESKTOP-VE2KLNM\sj <bsj6551@gmail.com>
2022-02-18 00:42:48 +0900

    '07'

---
+++ programmers/06.js
@@ -0,0 +1,34 @@
+function solution(board, moves) {
+    let answer = 0;
+    let basket = [];
+    let p1 = 0, p2 = 1, i = 0;
+    
+    for (let x of moves) {
+        while (i < board.length) {
+            if (board[i][x-1] !== 0) {
+                basket.push(board[i][x-1]);
+                board[i][x-1] = 0;
+                break;
+            }
+            i++;
+        }
+        i = 0;
+    }
+
+    while (p2 < basket.length) {
+        if (basket[p1] === basket[p2]) {
+            answer += 2;
+            basket.splice(p1,2);
+            p1 = 0, p2 = 1;
+        } else {
+            p1 += 1, p2 += 1;
+        }
+    }
+
+    return answer;
+}
+
+let board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]];
+let moves = [1,5,3,5,1,2,1,4];
+
+console.log(solution(board, moves));
--- section05/07.js
+++ section05/07.js
@@ -1,9 +1,20 @@
-const  = () => {
-    let answer;
-    
+const  anagram = (arr1, arr2) => {
+    let answer = 'YES';
+
+    const ana = new Map();
+    for (let x of arr1) {
+        ana.set(x, ana.get(x) + 1 || 1);
+    }
+
+    for (let x of arr2) {
+        if (!ana.has(x) || ana.get(x) === 0) return 'NO';
+        ana.set(x, ana.get(x) - 1);
+    }
+
     return answer;
 };
 
-arr = [];
+let arr1 = 'AbaAeCe';
+let arr2 = 'baeeACA';
 
-console.log();
+console.log(anagram(arr1, arr2));
