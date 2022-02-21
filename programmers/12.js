b110653f89fdced0f0c61e742e96cd03bed12d21
DESKTOP-VE2KLNM\sj <bsj6551@gmail.com>
2022-02-22 01:06:08 +0900

    '12'

---
+++ programmers/12.js
@@ -0,0 +1,16 @@
+const solution = (array, commands) => {
+    let answer = [];
+
+    for (let x of commands) { 
+        const [i, j, k] = x
+        let slicedArr = array.slice(i-1, j).sort((a,b) => a - b);
+        answer.push(slicedArr[k-1]);
+    }
+
+    return answer;
+};
+
+let array = [1, 5, 2, 6, 3, 7, 4];
+let commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]];
+
+console.log(solution(array, commands));
