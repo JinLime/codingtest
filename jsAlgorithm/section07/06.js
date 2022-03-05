4945e35a18ad20d479be1a50aa8b633346763944
sj <bsj6551@naver.com>
2022-03-05 16:54:29 +0900

    restructure02

--- jsAlgorithm/section07/06.js
+++ jsAlgorithm/section07/06.js
@@ -1,14 +1,9 @@
-function mischievousBoy(list) {
+function mischievousBoy(height) {
     let result = list;
-    for (let i = 0; i < list.length - 1; i++) {
-        for (let j = 0; j < list.length - i - 1; j++) {
-            if (list[j] > 0 && list[j + 1] < 0) {
-                [list[j], list[j + 1]] = [list[j + 1], list[j]];
-            }
-        }
-    }
+
     return result;
 }
 
 let list = [120, 125, 152, 130, 135, 135, 143, 127, 160];
+
 console.log(mischievousBoy(list));
