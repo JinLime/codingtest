9f9d17eb417844fafd713e888da4adef70fcfe79
DESKTOP-VE2KLNM\sj <bsj6551@gmail.com>
2022-03-06 01:34:29 +0900

    '01'

--- jsAlgorithm/section07/06.js
+++ jsAlgorithm/section07/06.js
@@ -1,12 +1,6 @@
 function mischievousBoy(list) {
     let result = list;
-    for (let i = 0; i < list.length - 1; i++) {
-        for (let j = 0; j < list.length - i - 1; j++) {
-            if (list[j] > 0 && list[j + 1] < 0) {
-                [list[j], list[j + 1]] = [list[j + 1], list[j]];
-            }
-        }
-    }
+    
     return result;
 }
 
