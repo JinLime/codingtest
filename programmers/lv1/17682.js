b25760231a9e770ac93dc481e4a3d08232cd4343
DESKTOP-VE2KLNM\sj <bsj6551@gmail.com>
2022-03-15 00:52:39 +0900

    '1'

---
+++ programmers/lv1/17682.js
@@ -0,0 +1,12 @@
+// const solution = () => {
+    
+// };
+
+// console.log(solution());
+
+let a = [1,3,2,4,5,6];
+
+a.sort((a,b) => {
+    console.log(a,b);
+    return a - b;
+})
--- programmers/lv1/82612.js
+++ programmers/lv1/82612.js
@@ -1,5 +1,6 @@
 const solution = function (price, money, count) {
     let cntArr = Array.from({ length: count }, (_, i) => (i + 1) * price).reduce((v, i) => v + i);
+    console.log(cntArr);
     return cntArr - money < 0 ? 0 : cntArr - money;
 };
 
