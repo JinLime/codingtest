7cd03cf08c4fd73d24a9858539c26588c579f4d2
sj <bsj6551@naver.com>
2022-04-30 14:48:57 +0900

    05 06 07

---
+++ jsAlgorithm/section09/06.js
@@ -0,0 +1,42 @@
+function solution(board) {
+    let result = 0;
+    let n = board.length;
+    let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+    let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+
+    function DFS(x, y) {
+        board[x][y] = 0;
+        for (let k = 0; k < dx.length; k++) {
+            let nx = x + dx[k];
+            let ny = y + dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+                console.log(nx, ny);
+                DFS(nx, ny);
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (board[i][j] === 1) {
+                result++;
+                console.log(i, j);
+                DFS(i, j);
+                console.log("DFS end");
+            }
+        }
+    }
+
+    return result;
+}
+
+let arr = [
+    [1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 1, 1, 0],
+    [0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0],
+    [1, 0, 1, 0, 1, 0, 0],
+];
+console.log(solution(arr));
